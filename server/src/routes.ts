5c7ee8d3c47ce7c54c600860500f1bfb766ed0c9
Maria P. Campos <mpfc.maria@gmail.com>
2020-08-04 16:36:54 -0300

    finishing projects api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,17 @@
 import express, { json } from 'express';
 import ClassesController from './controllers/ClassesControllers';
+import ConnectionsController from './controllers/ConnectionsControllers';
 
 
 const routes = express.Router();
 const classesControllers  = new ClassesController();
+const connectionsControllers = new ConnectionsController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.post('/connections', connectionsControllers.create)
+routes.get('/connections', connectionsControllers.index)
+
 
 export default routes;
