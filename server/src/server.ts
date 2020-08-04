0fe0755d7c4ef6d7bdcbe94b7eb6dde2e7a07101
Maria P. Campos <mpfc.maria@gmail.com>
2020-08-04 15:39:39 -0300

    adding route for creating a class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 
 const app = express();
 
 app.use(express.json());
+app.use(routes)
 
-
-app.get('/', (request, response) => {  
-
-    return response.json({
-        message: 'Hello World'
-    })
-})
 
 // localhost:3333
 app.listen(3333);
