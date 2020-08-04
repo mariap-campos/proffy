27bc9dd0d5078cca6b80674c8ceeb0e2f21c7800
Maria P. Campos <mpfc.maria@gmail.com>
2020-08-04 14:29:27 -0300

    config databse and creating users table

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (request, response) => {  
+
+    return response.json({
+        message: 'Hello World'
+    })
+})
+
+// localhost:3333
+app.listen(3333);
+
+
